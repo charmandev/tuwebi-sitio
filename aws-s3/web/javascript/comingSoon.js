c890d0ab56b33f868f8d5dee434befa063962075
Juanliendo22 <liendojuan070@gmail.com>
2024-04-28 15:30:35 -0300

    servicios responsive

---
+++ aws-s3/web/javascript/comingSoon.js
@@ -0,0 +1,30 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function prevSlide() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
+  }
+  showSlide(currentIndex);
+}
+
+function nextSlide() {
+  currentIndex++;
+  if (currentIndex >= slides.length) {
+    currentIndex = 0;
+  }
+  showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
